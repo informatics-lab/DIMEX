1145dd743d49caa7bf94887fc9e2fc473ec22624
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-01-27 16:10:42 +0000

    unit test sample_population function

--- R/sample_population.R
+++ R/sample_population.R
@@ -40,27 +40,27 @@ sample_population <- function(pop_dat,
   # Getting a list of strata for activities 
   lst_strata <- tus_dat %>% 
     # Grouping by stratification variables
-    group_by_at(.vars = tus_strata)%>% 
+    dplyr::group_by_at(.vars = tus_strata)%>% 
     # Summarising 
     dplyr::summarise(n = dplyr::n()) %>%
-    ungroup() %>%
+    dplyr::ungroup() %>%
     # Adding label to each strata
     dplyr::mutate(strata = 1:dplyr::n()) %>%
     dplyr::select(-c(n))
   # Getting activities ID and 
   tus_act_id <- tus_dat %>%
     # Merging on stratification labels
-    left_join(lst_strata,
+    dplyr::left_join(lst_strata,
               by = tus_strata) 
   # Normalising the weights within each strata
   tus_act_id <- tus_act_id %>%
     # Merging on summary of weights in each strata
-    left_join(tus_act_id%>%
+    dplyr::left_join(tus_act_id%>%
                 dplyr::group_by(strata) %>%
                 dplyr::summarise(sums = sum(weights)),
               by = 'strata') %>%
     # Normalising weights
-    mutate(weights = weights/sums) %>%
+    dplyr::mutate(weights = weights/sums) %>%
     # Removing unnecessary columns 
     dplyr::select(-c(sums))
   ##########################################
@@ -68,33 +68,33 @@ sample_population <- function(pop_dat,
   ##########################################
   # Sampling population to find exposures for
   pop_dat2 <- pop_dat %>%
-    group_by_at(.vars = pop_strata) %>%
-    sample_n(size = nsample, 
+    dplyr::group_by_at(.vars = pop_strata) %>%
+    dplyr::sample_n(size = nsample, 
              replace = FALSE)
   # Preparing shell dataset for sampling 
   activities <- expand.grid(pop_id = pop_dat2$pop_id,
                             date = seq(as.Date(start_date), as.Date(end_date), by = 1)) %>%
     # Adding on day information
-    mutate(day_label = weekdays(date),
+    dplyr::mutate(day_label = weekdays(date),
            day = as.numeric(factor(weekdays(date), levels = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))), 
-           daytype = case_when(day %in% c(1,7) ~ 1,
+           daytype = dplyr::case_when(day %in% c(1,7) ~ 1,
                                day %in% 2:6 ~ 2),
-           daytype_label = case_when(day %in% c(1,7) ~ 'Weekend',
+           daytype_label = dplyr::case_when(day %in% c(1,7) ~ 'Weekend',
                                      day %in% 2:6 ~ 'Weekday'),
-           season = case_when(month(date) %in% c(12, 1, 2) ~ 1,
-                              month(date) %in% c(3:5) ~ 2,
-                              month(date) %in% c(6:8) ~ 3,
-                              month(date) %in% c(9:11) ~ 4),
-           season_label = case_when(month(date) %in% c(12, 1, 2) ~ 'Winter',
-                                    month(date) %in% c(3:5) ~ 'Spring',
-                                    month(date) %in% c(6:8) ~ 'Summer',
-                                    month(date) %in% c(9:11) ~ 'Autumn')) %>%
+           season = dplyr::case_when(lubridate::month(date) %in% c(12, 1, 2) ~ 1,
+                              lubridate::month(date) %in% c(3:5) ~ 2,
+                              lubridate::month(date) %in% c(6:8) ~ 3,
+                              lubridate::month(date) %in% c(9:11) ~ 4),
+           season_label = dplyr::case_when(lubridate::month(date) %in% c(12, 1, 2) ~ 'Winter',
+                                    lubridate::month(date) %in% c(3:5) ~ 'Spring',
+                                    lubridate::month(date) %in% c(6:8) ~ 'Summer',
+                                    lubridate::month(date) %in% c(9:11) ~ 'Autumn')) %>%
     # Merging on population data
-    left_join(pop_dat %>%
-                select_at(c('pop_id', 'sex', 'agegr4', 'nssec5')),
+    dplyr::left_join(pop_dat %>%
+                dplyr::select_at(c('pop_id', 'sex', 'agegr4', 'nssec5')),
               by = 'pop_id') %>%
     # Merging on stratification labels
-    left_join(lst_strata,
+    dplyr::left_join(lst_strata,
               by = tus_strata)
   # Removing unecessary datasets
   rm(pop_dat2, lst_strata)
--- tests/testthat/test-sample_population.R
+++ tests/testthat/test-sample_population.R
@@ -1,6 +1,29 @@
 test_that("sample_population", {
-  pop_dat <- data.frame()
-  tus_dat <- data.frame()
-  actual <- sample_population(pop_dat, tus_dat)
+  
+  # Need columns: pop_id, sex, agegr4, nssec5
+  pop_dat <- data.frame(foo = c(1),
+                        pop_id = c(0),
+                        sex = c(0),
+                        agegr4 = c(0),
+                        nssec5 = c(0))
+  tus_dat <- data.frame(bar = c(1))
+  
+  nsample <- 1
+  weights <- NULL
+  
+  pop_strata <- c("foo")
+  tus_strata <- c("bar")
+  
+  start_date <- "2023-01-01"
+  end_date <- "2023-01-01"
+
+  actual <- sample_population(pop_dat,
+                              tus_dat,
+                              nsample,
+                              weights,
+                              pop_strata,
+                              tus_strata,
+                              start_date,
+                              end_date)
   expect_equal(actual, 4)
 })
