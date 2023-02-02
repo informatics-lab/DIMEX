06b7bdd95f8ecf2a88fd43773ee8cedccffd08c1
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-02 12:05:09 +0000

    Extract activity sampler code (#13)
    * begin to extract activity sampler code
    
    * begin to reason about the activity sampler
    
    * add pipe operator to package
    
    * add rest of 2_Activities_2021.R code
    
    * begin to wrap functionality in tests
    
    * fix data prep tests and ship location metadata with package
    
    * add data prep code to public interface
    
    * steadily working towards a minimal unit test
    
    * further refinement in search of adequate test data
    
    * failed attempt to satisfy population sampler
    
    * finally a suitable minimal test case to pass the population sample
    
    * wire up missing dependencies
    
    * build up expected value from end-to-end test
    
    * build up expected value from end-to-end test
    
    * green light test suite, with comments to explain short cuts
    
    * make magic number more explicit
    
    * make magic number more explicit
    
    * rename test and use 1-indexing

---
+++ R/activity_sampler_fill_in_missings.R
@@ -0,0 +1,75 @@
+#' Filling in missings in the Time Use Survey dataset
+#' using most popular activities
+#' 
+#' @param tus_dat - Time use survey data frame that needs processing
+#' @return Time use survey data frame
+#' @export
+fill_in_missings <- function(tus_dat) {
+  tus_dat <- tus_dat %>%
+    # Setting missings to NA
+    dplyr::mutate(location_popular = ifelse(location %in% c(-9, 0, 10, 99), NA, location)) %>%
+    # Merging on most popular location for each activity by strata
+    dplyr::left_join(tus_dat %>%
+                # Only keeping non-missing locations
+                dplyr::filter(!(location %in% c(-9, 0, 10, 99))) %>%
+                # Summarising by stratum and activity
+                dplyr::group_by_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity', 'location')) %>%
+                dplyr::summarise(n = length(location))  %>% 
+                dplyr::group_by_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity')) %>%
+                # Getting proportion of time spent in each location by activity
+                dplyr::mutate(p = n/sum(n),
+                              tmp = max(p)) %>%
+                # Only keeping most popular activity
+                dplyr::filter(p == tmp) %>%
+                # Keeping first if more than one selected
+                dplyr::mutate(tmp = min(location)) %>%
+                dplyr::filter(location == tmp) %>%
+                # Ungrouping
+                dplyr::ungroup() %>%
+                # Only keeping relevant columns
+                dplyr::select_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity', 'location')) %>%
+                # Renaming columns
+                dplyr::rename(tmp = location),
+              by = c('sex', 'agegr4', 'nssec5', 'daytype', 'activity'))  %>%
+    # Filling in the missings
+    dplyr::mutate(location_popular = ifelse(is.na(location_popular), 
+                                            tmp,
+                                            location_popular)) %>%
+    # Removing uncessary columns 
+    dplyr::select(-c(tmp))  %>%
+    # Merging on most popular location for each activity by strata
+    dplyr::left_join(tus_dat %>%
+                # Only keeping non-missing locations
+                dplyr::filter(!(location %in% c(-9, 0, 10, 99))) %>%
+                # Summarising by stratum and activity
+                dplyr::group_by_at(c('activity', 'location')) %>%
+                dplyr::summarise(n = length(location))  %>% 
+                dplyr::group_by_at(c('activity')) %>%
+                # Getting proportion of time spent in each location by activity
+                dplyr::mutate(p = n/sum(n),
+                              tmp = max(p)) %>%
+                # Only keeping most popular activity
+                dplyr::filter(p == tmp) %>%
+                # Keeping first if more than one selected
+                dplyr::mutate(tmp = min(location)) %>%
+                dplyr::filter(location == tmp) %>%
+                # Ungrouping
+                dplyr::ungroup() %>%
+                # Only keeping relevant columns
+                dplyr::select_at(c('activity', 'location')) %>%
+                # Renaming columns
+                dplyr::rename(tmp = location),
+              by = c('activity'))  %>%
+    # Filling in the missings
+    dplyr::mutate(location_popular = ifelse(is.na(location_popular), 
+                                            tmp,
+                                            location_popular)) %>%
+    # Removing uncessary columns 
+    dplyr::select(-c(tmp))%>%
+    # Merging on location labels
+    dplyr::left_join(uktus_metadata_location %>%
+                dplyr::select(location_popular = location, 
+                              location_popular_label = location_label),
+              by = 'location_popular')
+  tus_dat
+}
--- R/sample_population.R
+++ R/sample_population.R
@@ -55,10 +55,10 @@ sample_population <- function(pop_dat,
   # Normalising the weights within each strata
   tus_act_id <- tus_act_id %>%
     # Merging on summary of weights in each strata
-    dplyr::left_join(tus_act_id%>%
-                dplyr::group_by(strata) %>%
-                dplyr::summarise(sums = sum(weights)),
-              by = 'strata') %>%
+    dplyr::left_join(tus_act_id %>%
+                     dplyr::group_by(strata) %>%
+                     dplyr::summarise(sums = sum(weights)),
+                     by = 'strata') %>%
     # Normalising weights
     dplyr::mutate(weights = weights/sums) %>%
     # Removing unnecessary columns 
@@ -69,8 +69,8 @@ sample_population <- function(pop_dat,
   # Sampling population to find exposures for
   pop_dat2 <- pop_dat %>%
     dplyr::group_by_at(.vars = pop_strata) %>%
-    dplyr::sample_n(size = nsample, 
-             replace = FALSE)
+    dplyr::sample_n(size = nsample,
+                    replace = FALSE)
   # Preparing shell dataset for sampling 
   activities <- expand.grid(pop_id = pop_dat2$pop_id,
                             date = seq(as.Date(start_date), as.Date(end_date), by = 1)) %>%
@@ -78,9 +78,9 @@ sample_population <- function(pop_dat,
     dplyr::mutate(day_label = weekdays(date),
            day = as.numeric(factor(weekdays(date), levels = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))), 
            daytype = dplyr::case_when(day %in% c(1,7) ~ 1,
-                               day %in% 2:6 ~ 2),
+                                      day %in% 2:6 ~ 2),
            daytype_label = dplyr::case_when(day %in% c(1,7) ~ 'Weekend',
-                                     day %in% 2:6 ~ 'Weekday'),
+                                            day %in% 2:6 ~ 'Weekday'),
            season = dplyr::case_when(lubridate::month(date) %in% c(12, 1, 2) ~ 1,
                               lubridate::month(date) %in% c(3:5) ~ 2,
                               lubridate::month(date) %in% c(6:8) ~ 3,
@@ -91,17 +91,17 @@ sample_population <- function(pop_dat,
                                     lubridate::month(date) %in% c(9:11) ~ 'Autumn')) %>%
     # Merging on population data
     dplyr::left_join(pop_dat %>%
-                dplyr::select_at(c('pop_id', 'sex', 'agegr4', 'nssec5')),
-              by = 'pop_id') %>%
+                     dplyr::select_at(c('pop_id', 'sex', 'agegr4', 'nssec5')),
+                     by = 'pop_id') %>%
     # Merging on stratification labels
     dplyr::left_join(lst_strata,
-              by = tus_strata)
+                     by = tus_strata)
   # Removing unecessary datasets
   rm(pop_dat2, lst_strata)
   ###################################
   ### Sampling activity sequences ###
   ###################################
-  # Empty dataset 
+  # Empty dataset
   activities$act_id <- as.numeric(NA)
   # Loop for each strata
   for (i in unique(activities$strata)){
