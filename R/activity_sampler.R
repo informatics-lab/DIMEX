e2b5851682e9d700f56640665ac816bc9c69d59b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-02 14:41:40 +0000

    run styler on activity sampler code

--- R/activity_sampler.R
+++ R/activity_sampler.R
@@ -9,52 +9,67 @@
 activity_sampler <- function(pop_dat, tus_dat, k) {
   # TODO: Remove dependence on global variables, e.g. pop_dat, tus_dat
   # NOTE: Could the sampled population be passed in?
-  activities_complete <- sample_population(subset(pop_dat, area_id == k), 
-                                           subset(tus_dat, percmissing == 0), 
-                                           nsample = 100,
-                                           weights = "weights_diary",
-                                           pop_strata = c('area_id'),
-                                           tus_strata = c('sex', 'agegr4', 'nssec5', 'daytype'),
-                                           start_date = '2020-11-30',
-                                           end_date = '2021-12-31',
-                                           keep = c('activity', 'activity_label', 'location', 'location_label'))
-  
+  activities_complete <- sample_population(subset(pop_dat, area_id == k),
+    subset(tus_dat, percmissing == 0),
+    nsample = 100,
+    weights = "weights_diary",
+    pop_strata = c("area_id"),
+    tus_strata = c("sex", "agegr4", "nssec5", "daytype"),
+    start_date = "2020-11-30",
+    end_date = "2021-12-31",
+    keep = c("activity", "activity_label", "location", "location_label")
+  )
+
   # Activity sequences run from 04:00-03:59 so need to "shift"
   # the end of the profiles into the next day
   activities_complete <- activities_complete %>%
     # Getting hour and resting date after 00:00
-    dplyr::mutate(hour = (floor((time-1)/6) + 4) %% 24,
-           date = dplyr::if_else(hour %in% 0:3, date + 1, date)) %>%
+    dplyr::mutate(
+      hour = (floor((time - 1) / 6) + 4) %% 24,
+      date = dplyr::if_else(hour %in% 0:3, date + 1, date)
+    ) %>%
     # Removign day information as we have to shift the day
     dplyr::select(-c(day, day_label, daytype, daytype_label, season, season_label)) %>%
     # Adding on day information
-    dplyr::mutate(day_label = weekdays(date),
-           day = as.numeric(factor(weekdays(date), levels = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))), 
-           daytype = dplyr::case_when(day %in% c(1,7) ~ 1,
-                               day %in% 2:6 ~ 2),
-           daytype_label = dplyr::case_when(day %in% c(1,7) ~ 'Weekend',
-                                     day %in% 2:6 ~ 'Weekday'),
-           season = dplyr::case_when(lubridate::month(date) %in% c(12, 1, 2) ~ 1,
-                              lubridate::month(date) %in% c(3:5) ~ 2,
-                              lubridate::month(date) %in% c(6:8) ~ 3,
-                              lubridate::month(date) %in% c(9:11) ~ 4),
-           season_label = dplyr::case_when(lubridate::month(date) %in% c(12, 1, 2) ~ 'Winter',
-                                    lubridate::month(date) %in% c(3:5) ~ 'Spring',
-                                    lubridate::month(date) %in% c(6:8) ~ 'Summer',
-                                    lubridate::month(date) %in% c(9:11) ~ 'Autumn')) %>%
+    dplyr::mutate(
+      day_label = weekdays(date),
+      day = as.numeric(factor(weekdays(date), levels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))),
+      daytype = dplyr::case_when(
+        day %in% c(1, 7) ~ 1,
+        day %in% 2:6 ~ 2
+      ),
+      daytype_label = dplyr::case_when(
+        day %in% c(1, 7) ~ "Weekend",
+        day %in% 2:6 ~ "Weekday"
+      ),
+      season = dplyr::case_when(
+        lubridate::month(date) %in% c(12, 1, 2) ~ 1,
+        lubridate::month(date) %in% c(3:5) ~ 2,
+        lubridate::month(date) %in% c(6:8) ~ 3,
+        lubridate::month(date) %in% c(9:11) ~ 4
+      ),
+      season_label = dplyr::case_when(
+        lubridate::month(date) %in% c(12, 1, 2) ~ "Winter",
+        lubridate::month(date) %in% c(3:5) ~ "Spring",
+        lubridate::month(date) %in% c(6:8) ~ "Summer",
+        lubridate::month(date) %in% c(9:11) ~ "Autumn"
+      )
+    ) %>%
     # Removing first day
-    dplyr::filter(date >= as.Date('2020-12-01') & date <= as.Date('2021-12-31'))
-  
-  # Adding micro-environments to the dataset 
+    dplyr::filter(date >= as.Date("2020-12-01") & date <= as.Date("2021-12-31"))
+
+  # Adding micro-environments to the dataset
   activities_complete <- activities_complete %>%
-    dplyr::mutate(micro_group = dplyr::case_when(location %in% c(11:12) ~ "home",
-                                          location %in% c(13, 14, 15, 16, 17, 19, 20, 21) ~ "indoor",
-                                          location %in% c(18, 31, 32) ~ "outdoor",
-                                          location %in% c(30, 33:49, 90) ~ "transport"))
-  
+    dplyr::mutate(micro_group = dplyr::case_when(
+      location %in% c(11:12) ~ "home",
+      location %in% c(13, 14, 15, 16, 17, 19, 20, 21) ~ "indoor",
+      location %in% c(18, 31, 32) ~ "outdoor",
+      location %in% c(30, 33:49, 90) ~ "transport"
+    ))
+
   # Aggregating to the hourly time series
   activities_complete <- activities_complete %>%
-    # Getting time within hour 
+    # Getting time within hour
     dplyr::mutate(minutes = ((time - 1) %% 6) + 1) %>%
     # Grouping by population, date and hour to reduce
     dplyr::group_by(pop_id, date, hour) %>%
@@ -64,6 +79,6 @@ activity_sampler <- function(pop_dat, tus_dat, k) {
     dplyr::filter(minutes == sample) %>%
     # Removing unecesary columns
     dplyr::select(-c(minutes, sample))
-  
+
   activities_complete
 }
--- tests/testthat/test-activity_sampler.R
+++ tests/testthat/test-activity_sampler.R
@@ -2,42 +2,43 @@ test_that("activity_sampler works", {
   # WARNING: This is a long running test
 
   # Minimal population data.frame
-  msoa_id = 2
+  msoa_id <- 2
   n <- 100
   population <- data.frame(
-    area_id = rep(msoa_id, each = n),  # Sample size is hard-coded to 100
+    area_id = rep(msoa_id, each = n), # Sample size is hard-coded to 100
     pop_id = c(1:n),
     sex = rep(0, each = n),
     agegr4 = rep(0, each = n),
     nssec5 = rep(0, each = n)
   )
-  
+
   # Minimal Time-Use Survey data.frame
   # This is a very complex spec to track down by just reading the code
-  tus_dat <- data.frame(percmissing = rep(0, each = n),
-                        weights_diary = rep(1, each = n),
-                        sex = rep(0, each = n),
-                        agegr4 = rep(0, each = n),
-                        nssec5 = rep(0, each = n),
-                        pop_id = c(1:n),
-                        act_id = rep(1, each = n),
-                        daytype = rep(1:7, each = 15)[1:100],
-                        time = rep(0, each = n),
-                        time_label = rep(0, each = n),
-                        activity = rep(0, each = n),
-                        activity_label = rep(0, each = n),
-                        location = rep(0, each = n),
-                        location_label = rep(0, each = n)
-                        )
-  
+  tus_dat <- data.frame(
+    percmissing = rep(0, each = n),
+    weights_diary = rep(1, each = n),
+    sex = rep(0, each = n),
+    agegr4 = rep(0, each = n),
+    nssec5 = rep(0, each = n),
+    pop_id = c(1:n),
+    act_id = rep(1, each = n),
+    daytype = rep(1:7, each = 15)[1:100],
+    time = rep(0, each = n),
+    time_label = rep(0, each = n),
+    activity = rep(0, each = n),
+    activity_label = rep(0, each = n),
+    location = rep(0, each = n),
+    location_label = rep(0, each = n)
+  )
+
   # System under test
   set.seed(1409)
   actual <- activity_sampler(population, tus_dat, msoa_id)
 
   # Assertions
-  n_samples_magic_number <- 649100  # Sensitive to seed value
+  n_samples_magic_number <- 649100 # Sensitive to seed value
   ones <- rep(1, each = n_samples_magic_number)
-  
+
   # NOTE: these are not the actual values of the activity_sampler
   expected <- data.frame(
     act_id = ones,
