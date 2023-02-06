dc4ed94aa4dac9e1def01101fd94e6f6f45a794c
natalietf <natalie.trippfoulds@metoffice.gov.uk>
2023-02-06 15:25:36 +0000

    improving test

--- tests/testthat/test-estimate_exposures.R
+++ tests/testthat/test-estimate_exposures.R
@@ -1,39 +1,39 @@
 test_that("estimate_exposures", {
   
-  activities_complete <- data.frame(act_id = 11354,
-                                    pop_id = 13,
-                                    date = as.Date("2020-12-20"),
-                                    time = 122,
-                                    time_label = "00:10-00:20",
-                                    activity = 110,
-                                    activity_label = "Sleep",
-                                    location = 11,
-                                    location_label = "Home",
-                                    hour = 0,
-                                    day_label = "Sunday",
-                                    day = 1,
-                                    daytype = 1,
-                                    daytype_label = "Weekend",
-                                    season = 1,
-                                    season_label = "Winter",
-                                    micro_group = "home")
+  activities_complete <- data.frame(act_id = c(11354, 11354),
+                                    pop_id = c(13, 13),
+                                    date = as.Date(c("2020-12-20", "2020-12-20")),
+                                    time = c(122, 130),
+                                    time_label = c("00:10-00:20", "01:30-01:40"),
+                                    activity =  c(110),
+                                    activity_label = c("Sleep", "Sleep"),
+                                    location = c(11, 11),
+                                    location_label = c("Home", "Home"),
+                                    hour = c(0, 1),
+                                    day_label = c("Sunday", "Sunday"),
+                                    day = c(1, 1),
+                                    daytype = c(1, 1),
+                                    daytype_label = c("Weekend", "Weekend"),
+                                    season = c(1, 1),
+                                    season_label = c("Winter", "Winter"),
+                                    micro_group = c("home", "home"))
   
-  pop_dat <- data.frame(pop_id = c(0, 0),
-                        housetype = c(0, 0),
-                        area_id = c(0,0),
-                        sex = c(0,0),
-                        sex_label = c("female", "female"),
-                        agegr4 = c(1,2),
-                        agegr4_label = c("<16", "<16"),
-                        nssec5 = c(2,2),
-                        nssec5_label = c("Not applicable", "Not applicable"))
+  pop_dat <- data.frame(pop_id = c(0),
+                        housetype = c(0),
+                        area_id = c(0),
+                        sex = c(0),
+                        sex_label = c("female"),
+                        agegr4 = c(1),
+                        agegr4_label = c("<16"),
+                        nssec5 = c(2),
+                        nssec5_label = c("Not applicable"))
   
-  pm25_ctm <- data.frame(area_id = c(0,0),
-                         date = as.Date(c("2021-01-01", "2021-01-02")),
-                         hour = c(0, 0),
-                         pm25_cams_agg = c(5, 5),
-                         pm25_emep_agg = c(5, 5),
-                         pm25_five = c(5, 5))
+  pm25_ctm <- data.frame(area_id = c(0),
+                         date = as.Date(c("2021-01-01")),
+                         hour = c(0),
+                         pm25_cams_agg = c(5),
+                         pm25_emep_agg = c(5),
+                         pm25_five = c(5))
   
   actual <- estimate_exposures(activities_complete, pop_dat, pm25_ctm)
   
