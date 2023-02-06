71c3b854d7346c02652fd4f01d85b4af4b4be27c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-06 09:59:25 +0000

    add another test

--- tests/testthat/test-date-helpers.R
+++ tests/testthat/test-date-helpers.R
@@ -9,3 +9,11 @@ test_that("day_type handles bad data", {
   expect_true(is.na(day_type(0)))
   expect_true(is.na(day_type(8)))
 })
+
+test_that("day_number", {
+  dates <- seq(as.Date("2021-01-03"), by = "day", length.out = 7)
+  actual <- day_number(dates)
+  expected <- c(1, 2, 3, 4, 5, 6, 7)
+  expect_equal(actual, expected)
+  expect_equal(dates %>% weekdays, c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+})
