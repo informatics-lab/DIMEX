ce95c81efa3021c78b61e05e0828b5645f06753f
Peter Killick <peter.killick@informaticslab.co.uk>
2023-02-06 15:21:20 +0000

    WIP: Add section 4 code to package (#15)
    * Add section 4 code to package
    
    * Rename function
    
    * Add test file
    
    * Correct indentation
    
    * Longer list of required variable stubs
    
    * Remove variables that are actually colums
    
    * Add missing packages and function libraries
    
    * Add test input data
    
    * Daily average function working \o/
    
    * Doc update
    
    * Add expected values for functioning test
    
    * Broadcast and use test variables
    
    * Update test to confirm averages are being taken

---
+++ R/daily_averages.R
@@ -0,0 +1,36 @@
+#' Estimate daily average personal exposure for a specific MSOA.
+#'
+#' @param  activities_complete - sampled activities from earlier processing steps
+#' @return  dataframe of personal daily average exposure estimates
+#'
+#' @export
+daily_averages <- function(activities_complete) {
+  activities_complete <- activities_complete %>%
+    # Only keeping Q1 2021
+    dplyr::filter(date >= as.Date("2021-01-01")) %>%
+    # Getting exposures 
+    dplyr::mutate(exposure_cams = ifelse(micro_group == "outdoor", pm25_cams_agg,
+                                  ifelse(micro_group == "indoor", pm25_cams_agg_inh,
+                                         ifelse(micro_group == "transport", pm25_cams_agg_tns,
+                                                ifelse(micro_group == "home", pm25_cams_agg_hhd, NA)))),
+                  exposure_emep = ifelse(micro_group == "outdoor", pm25_emep_agg,
+                                         ifelse(micro_group == "indoor", pm25_emep_agg_inh,
+                                                ifelse(micro_group == "transport", pm25_emep_agg_tns,
+                                                       ifelse(micro_group == "home", pm25_emep_agg_hhd, NA)))),
+                  exposure_five = ifelse(micro_group == "outdoor", pm25_five,
+                                         ifelse(micro_group == "indoor", pm25_five_inh,
+                                                ifelse(micro_group == "transport", pm25_five_tns,
+                                                       ifelse(micro_group == "home", pm25_five_hhd, NA)))))%>%
+       # Averaging by day
+    plyr::ddply(plyr::.(area_id, pop_id, date, daytype, daytype_label, season, season_label,
+            sex, sex_label, agegr4, agegr4_label, nssec5, nssec5_label),
+          plyr::summarize,
+          exposure_cams = mean(exposure_cams),
+          exposure_emep = mean(exposure_emep),
+          exposure_five = mean(exposure_five),
+          pm25_cams_agg = mean(pm25_cams_agg),
+          pm25_emep_agg = mean(pm25_emep_agg),
+          pm25_five = mean(pm25_five))
+
+  activities_complete
+}
