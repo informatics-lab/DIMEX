c76fa90b980e177d181e1fdafe7aec1249b5c703
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-08 10:12:15 +0000

    extract micro_group logic

--- R/activity_sampler.R
+++ R/activity_sampler.R
@@ -54,12 +54,7 @@ activity_sampler <- function(
 
   # Adding micro-environments to the dataset
   activities_complete <- activities_complete %>%
-    dplyr::mutate(micro_group = dplyr::case_when(
-      location %in% c(11:12) ~ "home",
-      location %in% c(13, 14, 15, 16, 17, 19, 20, 21) ~ "indoor",
-      location %in% c(18, 31, 32) ~ "outdoor",
-      location %in% c(30, 33:49, 90) ~ "transport"
-    ))
+    dplyr::mutate(micro_group = micro_group(location))
 
   # Aggregating to the hourly time series
   activities_complete <- activities_complete %>%
