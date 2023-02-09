e0bbff01d00875321cfc243c38903bff3591b277
Natalie Tripp Foulds <83218927+natalietf@users.noreply.github.com>
2023-02-09 15:37:18 +0000

    WIP Migrate 3b exposures (#16)
    * estimate_exposure function extracted
    
    * estimate_exposure function extracted
    
    * renamed file and simplified to original code
    
    * added test
    
    * improving test
    
    * improving test
    
    * improving test
    
    * adding docs
    
    * shortening decimal place correction
    
    * editing comments

---
+++ R/estimate_exposures.R
@@ -0,0 +1,48 @@
+#' Estimates exposure for three groups (transport, indoor and household)
+#'
+#' @param activities_complete - output dataframe from activity sampler
+#' @param pop_dat - population dataframe
+#' @param pm25_ctm - merged dataframe made up of EMEP and CAMS data
+#' @return dataframe of completed activities
+#'
+#' @export
+
+estimate_exposures <- function(activities_complete, pop_dat, pm25_ctm) {
+  # Preparing data for exposure modelling
+  activities_complete <- activities_complete %>%
+    # Only keeping specific period
+    # TODO: convert date index to datetime format
+    dplyr::filter(as.numeric(date) >= 18616 &
+             as.numeric(date) <= 18717) %>%
+    # Adding on demographic variables
+    dplyr::left_join(pop_dat %>%
+                       dplyr::select(pop_id, area_id, sex, sex_label, agegr4, agegr4_label, nssec5, nssec5_label),
+                     by = 'pop_id') %>%
+    # Merging on pm data
+    dplyr::left_join(pm25_ctm %>%
+                       dplyr::select(area_id, date, hour, pm25_cams_agg, pm25_five, pm25_emep_agg),
+                     by = c('area_id', 'date', 'hour')) %>% 
+    as.data.frame()
+
+  # Transportation exposures
+  activities_complete <- calculate_transport(activities_complete, ambient = "pm25_cams_agg", outvar = "pm25_cams_agg_tns")
+  activities_complete <- calculate_transport(activities_complete, ambient = "pm25_emep_agg", outvar = "pm25_emep_agg_tns")
+  activities_complete <- calculate_transport(activities_complete, ambient = "pm25_five", outvar = "pm25_five_tns")
+  
+  # Indoor-not-home exposures
+  activities_complete <- calculate_indoor(activities_complete, ambient = "pm25_cams_agg", outvar = "pm25_cams_agg_inh")
+  activities_complete <- calculate_indoor(activities_complete, ambient = "pm25_emep_agg", outvar = "pm25_emep_agg_inh")
+  activities_complete <- calculate_indoor(activities_complete, ambient = "pm25_five", outvar = "pm25_five_inh")
+  
+  # Household exposures
+  activities_complete <- calculate_household(act_dat = activities_complete, pop_dat = pop_dat, 
+                                             ambient = "pm25_cams_agg", outvar = "pm25_cams_agg_hhd")
+  activities_complete <- calculate_household(act_dat = activities_complete, pop_dat = pop_dat, 
+                                             ambient = "pm25_emep_agg", outvar = "pm25_emep_agg_hhd")
+  activities_complete <- calculate_household(act_dat = activities_complete, pop_dat = pop_dat, 
+                                             ambient = "pm25_five", outvar = "pm25_five_hhd")
+  activities_complete
+}
+
+
+
---
+++ tests/testthat/test-estimate_exposures.R
@@ -0,0 +1,93 @@
+test_that("estimate_exposures works", {
+  
+  # Example activities data frame including all columns (all required)
+  activities_complete <- data.frame(act_id = c(11354, 13473),
+                                    pop_id = c(13, 13),
+                                    date = as.Date(c("2020-12-20", "2020-12-20")),
+                                    time = c(122, 130),
+                                    time_label = c("00:10-00:20", "01:30-01:40"),
+                                    activity =  c(110, 110),
+                                    activity_label = c("Sleep", "Sleep"),
+                                    location = c(11, 11),
+                                    location_label = c("Home", "Home"),
+                                    hour = c(0, 0),
+                                    day_label = c("Sunday", "Monday"),
+                                    day = c(1, 2),
+                                    daytype = c(1, 2),
+                                    daytype_label = c("Weekend", "Weekday"),
+                                    season = c(1, 1),
+                                    season_label = c("Winter", "Winter"),
+                                    micro_group = c("home", "home"))
+  
+  # Example population data frame containing only required columns
+  pop_dat <- data.frame(pop_id = 13,
+                        housetype = "semi-detached",
+                        area_id = "E02000984",
+                        agegr4 = 4,
+                        agegr4_label = "45-59",
+                        sex = 1,
+                        sex_label = "Male",
+                        nssec5 = 2,
+                        nssec5_label = "Intermediate occupations")
+  
+  # Example merged EMEP and CAMS PM2.5 data frame containing all columns (all required)
+  pm25_ctm <- data.frame(area_id = "E02000984",
+                         date = as.Date("2020-12-20"),
+                         hour = 0,
+                         pm25_cams_agg = 5,
+                         pm25_emep_agg = 5,
+                         pm25_five = 5)
+  
+  # Setting the seed
+  set.seed(1409)
+  
+  actual <- estimate_exposures(activities_complete, pop_dat, pm25_ctm)
+  
+  # Change results to two decimal places in order to test against expected values
+  exposures <- c("pm25_cams_agg_tns", "pm25_emep_agg_tns", "pm25_five_tns",
+                 "pm25_cams_agg_inh", "pm25_emep_agg_inh", "pm25_five_inh",
+                 "pm25_cams_agg_hhd", "pm25_emep_agg_hhd", "pm25_five_hhd")
+  
+  actual[, exposures] <- round(actual[, exposures], 2)
+  
+  expected <- data.frame(act_id = c(11354, 13473),
+                         pop_id = c(13, 13),
+                         date = as.Date(c("2020-12-20", "2020-12-20")),
+                         time = c(122, 130),
+                         time_label = c("00:10-00:20", "01:30-01:40"),
+                         activity =  c(110, 110),
+                         activity_label = c("Sleep", "Sleep"),
+                         location = c(11, 11),
+                         location_label = c("Home", "Home"),
+                         hour = c(0, 0),
+                         day_label = c("Sunday", "Monday"),
+                         day = c(1, 2),
+                         daytype = c(1, 2),
+                         daytype_label = c("Weekend", "Weekday"),
+                         season = c(1, 1),
+                         season_label = c("Winter", "Winter"),
+                         micro_group = c("home", "home"),
+                         area_id = c("E02000984", "E02000984"),
+                         sex = c(1, 1),
+                         sex_label = c("Male", "Male"),
+                         agegr4 = c(4, 4),
+                         agegr4_label = c("45-59", "45-59"),
+                         nssec5 = c(2, 2),
+                         nssec5_label = c("Intermediate occupations", "Intermediate occupations"), 
+                         pm25_cams_agg = c(5, 5),
+                         pm25_five = c(5, 5),
+                         pm25_emep_agg = c(5, 5),
+                         pm25_cams_agg_tns = c(28.65, 34.63),
+                         pm25_emep_agg_tns = c(33.44, 36.13), 
+                         pm25_five_tns = c(25.57, 12.45),
+                         pm25_cams_agg_inh = c(11.58, 7.69),
+                         pm25_emep_agg_inh = c(7.09, 10.24),
+                         pm25_five_inh = c(10.79, 6.36),
+                         uniid = c(1, 2),
+                         pm25_cams_agg_hhd = c(14.63, 6.60), 
+                         pm25_emep_agg_hhd = c(13.42, 8.99),
+                         pm25_five_hhd = c(10.08, 5.72))
+  
+  expect_equal(actual, expected)
+  
+})
